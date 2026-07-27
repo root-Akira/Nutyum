735a7fa251aecc5d70978f0cf071226dc895e674
root-Akira <sunny.singh04@proton.me>
2026-07-27 17:09:43 +0530

    favicons: transparent bg, multiple sizes

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,12 +23,12 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-192x192.png", sizes: "192x192", type: "image/png" },
-      { url: "/favicon.png", sizes: "512x512", type: "image/png" },
+      { url: "/favicon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
     shortcut: "/favicon-32x32.png",
     apple: [
       { url: "/favicon-192x192.png", sizes: "192x192", type: "image/png" },
-      { url: "/favicon.png", sizes: "512x512", type: "image/png" },
+      { url: "/favicon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
   },
 };
