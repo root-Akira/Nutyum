03b974cbc26efe9a93b89344b3d11dfba8af3030
root-Akira <sunny.singh04@proton.me>
2026-07-27 15:44:54 +0530

    increase navbar logo to 100px

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -290,10 +290,10 @@ function MobileDrawer({ isOpen, onClose, session }: { isOpen: boolean; onClose: 
               <Image
                 src="/logo.png"
                 alt="Nutyum"
-                width={200}
-                height={80}
+                width={260}
+                height={100}
                 className="object-contain"
-                style={{ height: "80px", width: "auto" }}
+                style={{ height: "100px", width: "auto" }}
               />
               <button onClick={onClose} aria-label="Close menu" className="p-1 text-[#173D22]">
                 <X size={20} strokeWidth={1.6} />
